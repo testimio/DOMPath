10d5c111b5e01e88ffc8aee6dc3fa986d241bd20
Ran Cohen <rannn505@outlook.com>
2019-07-21 17:37:17 +0300

    add types

---
+++ lib/DOMPath.d.ts
@@ -0,0 +1,6 @@
+export function fullQualifiedSelector(node: Element, justSelector: boolean): string;
+
+export function jsPath(node: Element, optimized: boolean): string;
+
+export function xPath(node: Element, optimized: boolean): string;
+
